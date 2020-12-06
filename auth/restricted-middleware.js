bfeca5cc7c6ac3e07703863d6bbe4925767cbc3e
angelynaliem <angelynaliem@gmail.com>
2020-12-05 23:12:14 -0500

    created restricted middleware

---
+++ auth/restricted-middleware.js
@@ -0,0 +1,9 @@
+const server = require("../api/server");
+
+module.exports = (req, res, next) => {
+  if (req.session && req.session.user) {
+    next();
+  } else {
+    res.status(401).json({ message: "you shall not pass" });
+  }
+};
